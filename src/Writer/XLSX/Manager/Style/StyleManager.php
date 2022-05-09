andreym333/openspout

commit 811f8e8e3769f1f75472e4ea0f64c236cc05f1f5

Improve write performance

src/Writer/XLSX/Manager/WorksheetManager.php
@@ -39,7 +39,7 @@ class WorksheetManager implements WorksheetManagerInterface
     public const SHEET_XML_FILE_HEADER = <<<'EOD'
         <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
         <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-        EOD;
+EOD;
 
     /** @var bool Whether inline or shared strings should be used */
     protected $shouldUseInlineStrings;
@@ -69,10 +69,13 @@ class WorksheetManager implements WorksheetManagerInterface
     private $stringHelper;
 
     /** @var array */
-    private $columnLetters = [];
+    private $columnLettersCache = [];
 
     /** @var array */
-    private $registeredStyles = [];
+    private $registeredStylesCache = [];
+
+    /** @var array */
+    private $shouldApplyStyleOnEmptyCellCache = [];
 
     /**
      * WorksheetManager constructor.
@@ -276,25 +279,29 @@ class WorksheetManager implements WorksheetManagerInterface
             }
 
             $serializedStyle = $newCellStyle->serialize();
-            if (!isset($this->registeredStyles[$serializedStyle])) {
-                $this->registeredStyles[$serializedStyle] = $this->styleManager->registerStyle($newCellStyle);
+            if (!isset($this->registeredStylesCache[$serializedStyle])) {
+                $this->registeredStylesCache[$serializedStyle] = $this->styleManager->registerStyle($newCellStyle);
             }
 
-            $cellStyle = $this->registeredStyles[$serializedStyle];
+            $cellStyle = $this->registeredStylesCache[$serializedStyle];
             if ($isMatchingRowStyle) {
                 $rowStyle = $cellStyle; // Replace actual rowStyle (possibly with null id) by registered style (with id)
             }
 
             // Generate the cell XML content
 
-            if (!isset($this->columnLetters[$columnIndexZeroBased])) {
-                $this->columnLetters[$columnIndexZeroBased] = CellHelper::getColumnLettersFromColumnIndex($columnIndexZeroBased);
-            }
-            $columnLetters = $this->columnLetters[$columnIndexZeroBased];
-            $cellXML = '<c r="'.$columnLetters.$rowIndexOneBased.'"';
-            $cellXML .= ' s="'.$cellStyle->getId().'"';
+            $cellType = $cell->getType();
+            $styleId = $cellStyle->getId();
 
-            if ($cell->isString()) {
+            if (!isset($this->columnLettersCache[$columnIndexZeroBased])) {
+                $this->columnLettersCache[$columnIndexZeroBased] = CellHelper::getColumnLettersFromColumnIndex($columnIndexZeroBased);
+            }
+            $columnLetters = $this->columnLettersCache[$columnIndexZeroBased];
+
+            $cellXML = '<c r="'.$columnLetters.$rowIndexOneBased.'"';
+            $cellXML .= ' s="'.$styleId.'"';
+
+            if ($cellType === Cell::TYPE_STRING) {
                 $value = $cell->getValue();
                 if (\strlen($value) > self::MAX_CHARACTERS_PER_CELL && $this->stringHelper->getStringLength($value) > self::MAX_CHARACTERS_PER_CELL) {
                     throw new InvalidArgumentException('Trying to add a value that exceeds the maximum number of characters allowed in a cell (32,767)');
@@ -306,24 +313,28 @@ class WorksheetManager implements WorksheetManagerInterface
                     $sharedStringId = $this->sharedStringsManager->writeString($value);
                     $cellXML .= ' t="s"><v>'.$sharedStringId.'</v></c>';
                 }
-            } elseif ($cell->isBoolean()) {
+            } elseif ($cellType === Cell::TYPE_BOOLEAN) {
                 $cellXML .= ' t="b"><v>'.(int) ($cell->getValue()).'</v></c>';
-            } elseif ($cell->isNumeric()) {
+            } elseif ($cellType === Cell::TYPE_NUMERIC) {
                 $cellXML .= '><v>'.$this->stringHelper->formatNumericValue($cell->getValue()).'</v></c>';
-            } elseif ($cell->isFormula()) {
+            } elseif ($cellType === Cell::TYPE_FORMULA) {
                 $cellXML .= '><f>'.substr($cell->getValue(), 1).'</f></c>';
-            } elseif ($cell->isDate()) {
+            } elseif ($cellType === Cell::TYPE_DATE) {
                 $value = $cell->getValue();
                 if ($value instanceof \DateTimeInterface) {
                     $cellXML .= '><v>'.(string) DateHelper::toExcel($value).'</v></c>';
                 } else {
                     throw new InvalidArgumentException('Trying to add a date value with an unsupported type: '.\gettype($value));
                 }
-            } elseif ($cell->isError() && \is_string($cell->getValueEvenIfError())) {
+            } elseif ($cellType === Cell::TYPE_ERROR && \is_string($cell->getValueEvenIfError())) {
                 // only writes the error value if it's a string
                 $cellXML .= ' t="e"><v>'.$cell->getValueEvenIfError().'</v></c>';
-            } elseif ($cell->isEmpty()) {
-                if ($this->styleManager->shouldApplyStyleOnEmptyCell($styleId)) {
+            } elseif ($cellType === Cell::TYPE_EMPTY) {
+                if (!isset($this->shouldApplyStyleOnEmptyCellCache[$styleId])) {
+                    $this->shouldApplyStyleOnEmptyCellCache[$styleId] = $this->styleManager->shouldApplyStyleOnEmptyCell($styleId);
+                }
+
+                if ($this->shouldApplyStyleOnEmptyCellCache[$styleId]) {
                     $cellXML .= '/>';
                 } else {
                     // don't write empty cells that do no need styling

src/Writer/ODS/Manager/WorksheetManager.php
@@ -37,7 +37,7 @@ class WorksheetManager implements WorksheetManagerInterface
     private $styleMerger;
 
     /** @var array */
-    private $registeredStyles = [];
+    private $registeredStylesCache = [];
 
     /**
      * WorksheetManager constructor.
@@ -137,25 +137,28 @@ class WorksheetManager implements WorksheetManagerInterface
                 }
 
                 $serializedStyle = $newCellStyle->serialize();
-                if (!isset($this->registeredStyles[$serializedStyle])) {
-                    $this->registeredStyles[$serializedStyle] = $this->styleManager->registerStyle($newCellStyle);
+                if (!isset($this->registeredStylesCache[$serializedStyle])) {
+                    $this->registeredStylesCache[$serializedStyle] = $this->styleManager->registerStyle($newCellStyle);
                 }
 
-                $cellStyle = $this->registeredStyles[$serializedStyle];
+                $cellStyle = $this->registeredStylesCache[$serializedStyle];
                 if ($isMatchingRowStyle) {
                     $rowStyle = $cellStyle; // Replace actual rowStyle (possibly with null id) by registered style (with id)
                 }
 
                 // Generate the cell XML content
 
-                $data .= '<table:table-cell table:style-name="ce'.($cellStyle->getId() + 1).'"';
+                $cellType = $cell->getType();
+                $styleId = $cellStyle->getId();
+
+                $data .= '<table:table-cell table:style-name="ce'.($styleId + 1).'"';
 
                 $numTimesValueRepeated = $nextCellIndex - $currentCellIndex;
                 if (1 !== $numTimesValueRepeated) {
                     $data .= ' table:number-columns-repeated="'.$numTimesValueRepeated.'"';
                 }
 
-                if ($cell->isString()) {
+                if ($cellType === Cell::TYPE_STRING) {
                     $data .= ' office:value-type="string" calcext:value-type="string">';
 
                     $cellValueLines = explode("\n", $cellValue);
@@ -164,17 +167,17 @@ class WorksheetManager implements WorksheetManagerInterface
                     }
 
                     $data .= '</table:table-cell>';
-                } elseif ($cell->isBoolean()) {
+                } elseif ($cellType === Cell::TYPE_BOOLEAN) {
                     $value = $cellValue ? 'true' : 'false'; // boolean-value spec: http://docs.oasis-open.org/office/v1.2/os/OpenDocument-v1.2-os-part1.html#datatype-boolean
                     $data .= ' office:value-type="boolean" calcext:value-type="boolean" office:boolean-value="'.$value.'">';
                     $data .= '<text:p>'.$cellValue.'</text:p>';
                     $data .= '</table:table-cell>';
-                } elseif ($cell->isNumeric()) {
+                } elseif ($cellType === Cell::TYPE_NUMERIC) {
                     $cellValue = $this->stringHelper->formatNumericValue($cellValue);
                     $data .= ' office:value-type="float" calcext:value-type="float" office:value="'.$cellValue.'">';
                     $data .= '<text:p>'.$cellValue.'</text:p>';
                     $data .= '</table:table-cell>';
-                } elseif ($cell->isDate()) {
+                } elseif ($cellType === Cell::TYPE_DATE) {
                     $value = $cellValue;
                     if ($value instanceof \DateTimeInterface) {
                         $datevalue = substr((new \DateTimeImmutable('@'.$value->getTimestamp()))->format(\DateTimeInterface::W3C), 0, -6);
@@ -191,12 +194,12 @@ class WorksheetManager implements WorksheetManagerInterface
                         throw new InvalidArgumentException('Trying to add a date value with an unsupported type: '.\gettype($cellValue));
                     }
                     $data .= '</table:table-cell>';
-                } elseif ($cell->isError() && \is_string($cell->getValueEvenIfError())) {
+                } elseif ($cellType === Cell::TYPE_ERROR && \is_string($cell->getValueEvenIfError())) {
                     // only writes the error value if it's a string
                     $data .= ' office:value-type="string" calcext:value-type="error" office:value="">';
                     $data .= '<text:p>'.$cell->getValueEvenIfError().'</text:p>';
                     $data .= '</table:table-cell>';
-                } elseif ($cell->isEmpty()) {
+                } elseif ($cellType === Cell::TYPE_EMPTY) {
                     $data .= '/>';
                 } else {
                     throw new InvalidArgumentException('Trying to add a value with an unsupported type: '.\gettype($cellValue));

commit bbc0b2ee510ea997b814749e7f1d34cd7add709f

Fix bugs
1. The formatCode attribute can now contain quotes

2. Fixed bug in SHEET_XML_FILE_HEADER constant (broken in commit 811f8e8)

src/Writer/XLSX/Manager/Style/StyleManager.php
@@ -5,6 +5,7 @@ namespace OpenSpout\Writer\XLSX\Manager\Style;
 use OpenSpout\Common\Entity\Style\BorderPart;
 use OpenSpout\Common\Entity\Style\Color;
 use OpenSpout\Common\Entity\Style\Style;
+use OpenSpout\Common\Helper\Escaper;
 use OpenSpout\Writer\XLSX\Helper\BorderHelper;
 
 /**
@@ -87,7 +88,7 @@ class StyleManager extends \OpenSpout\Writer\Common\Manager\Style\StyleManager
             /** @var Style $style */
             $style = $this->styleRegistry->getStyleFromStyleId($styleId);
             $format = $style->getFormat();
-            $tags[] = '<numFmt numFmtId="'.$numFmtId.'" formatCode="'.$format.'"/>';
+            $tags[] = '<numFmt numFmtId="'.$numFmtId.'" formatCode="'.Escaper\XLSX::escape($format).'"/>';
         }
         $content = '<numFmts count="'.\count($tags).'">';
         $content .= implode('', $tags);

src/Writer/XLSX/Manager/WorksheetManager.php
@@ -39,7 +39,7 @@ class WorksheetManager implements WorksheetManagerInterface
     public const SHEET_XML_FILE_HEADER = <<<'EOD'
         <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
         <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
-EOD;
+        EOD;
 
     /** @var bool Whether inline or shared strings should be used */
     protected $shouldUseInlineStrings;